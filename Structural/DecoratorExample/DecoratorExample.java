44bad59b3cb8ccd2113ed9f2bd0647b726091634
qwerty7989 <nattapat.ungk@gmail.com>
2023-02-08 14:43:07 +0700

    add Decorator(Structural)

--- Structural/DecoratorExample/DecoratorExample.java
+++ Structural/DecoratorExample/DecoratorExample.java
@@ -1,23 +1,84 @@
 package DecoratorExample;
 
-class Shape {
-
+interface Shape {
+    String info();
 }
 
 class Circle implements Shape {
-    private radius;
+    private float radius;
 
-    @Override String info() {
+    public Circle() {}
+
+    public Circle(float radius) {
+        this.radius = radius;
+    }
+
+    void resize(float factor) {
+        radius *= factor;
+    }
+
+    @Override
+    public String info() {
         return "A circle of radius " + radius;
     }
 }
 
+class Square implements Shape {
+    private float side;
 
+    public Square() {}
+
+    public Square(float side) {
+        this.side = side;
+    }
+
+    @Override
+    public String info() {
+        return "A square with side " + side;
+    }
+}
+
+class ColorDecorator implements Shape {
+    private Shape shape;
+    private String color;
+
+    public ColorDecorator(Shape shape, String color) {
+        this.shape = shape;
+        this.color = color;
+    }
+
+    @Override
+    public String info() {
+        return shape.info() + " has the color " + color;
+    }
+}
+
+class TransparentDecorator implements Shape {
+    private Shape shape;
+    private int transparency;
+
+    public TransparentDecorator(Shape shape, int transparency) {
+        this.shape = shape;
+        this.transparency = transparency;
+    }
+
+    @Override
+    public String info() {
+        return shape.info() + " has " + transparency + "% transparency";
+    }
+}
 
 class DecoratorExample {
     public static void main(String[] args) {
-        Circle cirle = new Circle();
-        
-    }
-    
+        Circle circle = new Circle(10);
+        System.out.println(circle.info());
+        circle.resize(2);
+        System.out.println(circle.info());
+
+        ColorDecorator blueSquare = new ColorDecorator(new Square(20), "blue");
+        System.out.println(blueSquare.info());
+
+        TransparentDecorator newCircle = new TransparentDecorator(new ColorDecorator(new Circle(5), "green"), 50);
+        System.out.println(newCircle.info());
+    }    
 }
